414eac288239d7f02f884d0a285c70a42b466e95
Daniel Vella <bubli@hotmail.com>
2024-03-03 17:11:46 +0100

    Unit 2 complete

---
+++ UNIT2/sc-bed-simplephpsite-complete/phpinfo.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include("includes/head.php"); ?>
+
+<body>
+    <?php include("includes/menu.php"); ?>
+    <div class="container">
+        <?php phpinfo(); ?>
+    </div>
+    <?php include("includes/footer.php"); ?>
+</body>
+</html>
---
+++ UNIT2/sc-bed-simplephpsite-complete/phpsummary.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include("includes/head.php"); ?>
+
+<body>
+    <?php include("includes/menu.php"); ?>
+    <div class="container">
+        <h1>PHP Summary</h1>
+        <?php
+            printf('<p>You are running PHP version %s', PHPVERSION());
+
+            echo '<h4>Loaded Extensions</h4>';
+            $ext = get_loaded_extensions();
+            var_dump( $ext);
+
+            echo '<h4>Server Enviroment</h4>';
+            var_dump($_SERVER);
+        ?>
+    </div>
+    <?php include("includes/footer.php"); ?>
+</body>
+</html>
